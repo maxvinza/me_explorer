a66c9593583770d83f3303f9d8a9c98bf06efa76
Maxim Vinogradov <maxvinza@yandex.ru>
2020-10-16 09:26:46 +0200

    change catalogs and migrate to vue3

---
+++ client/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <commutator :config="config"></commutator>
+</template>
+
+<script>
+import Commutator from './components/commutator.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Commutator
+    }
+}
+</script>
--- php/tests/snmp.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once '../snmp_interface.php';
-$test3200 = new SNMP_320026_EMUL("10.10.99.99");
-var_dump($test3200->device_all_info());
